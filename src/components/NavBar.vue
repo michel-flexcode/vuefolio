da1b99215ce9a71b4944a1ede622641254c2475c
michel-flexcode <michelseocode@gmail.com>
2023-11-20 22:13:00 +0100

    V3 petites modifs

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,9 @@
 <template>
-  <nav class="">
-    <router-link to="/">Home</router-link>
-    <router-link to="/Project">Projets</router-link>
-    <router-link to="/about">About</router-link>
+    <!-- Test des hovers sur "/" -->
+
+  <nav class="bg-blue-500 p-4 flex items-center justify-center">
+    <router-link to="/" class="text-white mx-4 hover:bg-sky-700">Home</router-link>
+    <router-link to="/project" class="text-white mx-4">Projets</router-link>
+    <router-link to="/about" class="text-white mx-4">About</router-link>
   </nav>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,12 @@
         <h1 class="text-4xl font-bold mb-4">
           Hello, je m'appelle Michel Flexcode
         </h1>
-        <p>Je suis étudiant</p>
+        <p>Je suis étudiant en webdev à l'IFOSUP Wavre, je développe des projets en front et back end</p>
+        <p>Mes stacks principales sont Vuejs, Laravel, Wordpress</p>
+        <p>En tant que développeur j'ai aussi des compétences annexes : création de base de données, mise en ligne de projets...</p>
+
+        <h2>Pour plus d'informations envoyez-moi un mail !</h2>
+        <a href="mailto:votre@email.com" class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Contactez-moi</a>
       </div>
 
       <div>
