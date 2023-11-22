a118ad4676ccca092efc0f9e5f641b894f612acf
MichelCode <michelseocode.com>
2023-11-22 14:30:26 +0100

    V5 last functionnal

---
+++ src/components/ContactFromComponent.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1 class="bg-blue-500">
+    <slot></slot>
+  </h1>
+</template>
--- src/components/SearchAutocomplete.vue
+++ src/components/SearchAutocomplete.vue
@@ -1,9 +1,24 @@
 <!-- https://www.digitalocean.com/community/tutorials/vuejs-vue-autocomplete-component -->
 <template>
   <div class="autocomplete">
-    <input v-model="search" @input="onChange" type="text" />
-    <ul v-show="isOpen" class="autocomplete-results">
-      <li v-for="(result, i) in results" :key="i" class="autocomplete-result">
+    <input
+      type="text"
+      @input="onChange"
+      v-model="search"
+      @keydown.down="onArrowDown"
+      @keydown.up="onArrowUp"
+      @keydown.enter="onEnter"
+    />
+    <ul id="autocomplete-results" v-show="isOpen" class="autocomplete-results">
+      <li class="loading" v-if="isLoading">Loading results...</li>
+      <li
+        v-else
+        v-for="(result, i) in results"
+        :key="i"
+        @click="setResult(result)"
+        class="autocomplete-result"
+        :class="{ 'is-active': i === arrowCounter }"
+      >
         {{ result }}
       </li>
     </ul>
@@ -19,24 +34,103 @@ export default {
       required: false,
       default: () => [],
     },
+    isAsync: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
   },
   data() {
     return {
-      search: "",
-      results: [],
       isOpen: false,
+      results: [],
+      search: "",
+      isLoading: false,
+      arrowCounter: -1,
     };
   },
+  watch: {
+    items: function (value, oldValue) {
+      if (value.length !== oldValue.length) {
+        this.results = value;
+        this.isLoading = false;
+      }
+    },
+  },
+  mounted() {
+    document.addEventListener("click", this.handleClickOutside);
+  },
+  destroyed() {
+    document.removeEventListener("click", this.handleClickOutside);
+  },
   methods: {
+    setResult(result) {
+      this.search = result;
+      this.isOpen = false;
+    },
     filterResults() {
-      this.results = this.items.filter(
-        (item) => item.toLowerCase().indexOf(this.search.toLowerCase()) > -1
-      );
+      this.results = this.items.filter((item) => {
+        return item.toLowerCase().indexOf(this.search.toLowerCase()) > -1;
+      });
     },
     onChange() {
-      this.filterResults();
-      this.isOpen = true;
+      this.$emit("input", this.search);
+
+      if (this.isAsync) {
+        this.isLoading = true;
+      } else {
+        this.filterResults();
+        this.isOpen = true;
+      }
+    },
+    handleClickOutside(event) {
+      if (!this.$el.contains(event.target)) {
+        this.isOpen = false;
+        this.arrowCounter = -1;
+      }
+    },
+    onArrowDown() {
+      if (this.arrowCounter < this.results.length) {
+        this.arrowCounter = this.arrowCounter + 1;
+      }
+    },
+    onArrowUp() {
+      if (this.arrowCounter > 0) {
+        this.arrowCounter = this.arrowCounter - 1;
+      }
+    },
+    onEnter() {
+      this.search = this.results[this.arrowCounter];
+      this.isOpen = false;
+      this.arrowCounter = -1;
     },
   },
 };
 </script>
+
+<style>
+.autocomplete {
+  position: relative;
+}
+
+.autocomplete-results {
+  padding: 0;
+  margin: 0;
+  border: 1px solid #eeeeee;
+  height: 120px;
+  overflow: auto;
+}
+
+.autocomplete-result {
+  list-style: none;
+  text-align: left;
+  padding: 4px 2px;
+  cursor: pointer;
+}
+
+.autocomplete-result.is-active,
+.autocomplete-result:hover {
+  background-color: #4aae9b;
+  color: white;
+}
+</style>
